c4664b2678ccae0363fea42628a0cd06e99dbda0
Ilya Kolebayev <kolebayev@gmail.com>
2023-05-12 22:37:21 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,16 @@
 import Head from 'next/head';
-
 import Converter from '../components/Converter/Converter';
 import LayoutWrapper from '@/components/LayoutWrapper/LayoutWrapper';
-
-import Header from '../components/Header';
-
-
 
 export default function Home() {
   return (
     <>
       <Head>
-
         <title>The EVM Swiss Army Knife — Chainstack</title>
         <meta
           name="description"
           content="The ultimate EVM tool, convert and encode EVM values, all in one
             place"
-
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
